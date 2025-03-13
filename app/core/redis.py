893a7ad643c3b57758e9a3c077d9795dec5d544b
Ramis <ramis@Ramiss-Air.Dlink>
2025-03-13 16:49:45 +0300

    MVP

--- app/core/redis.py
+++ app/core/redis.py
@@ -3,7 +3,7 @@ from redis.asyncio import Redis
 
 async def get_redis() -> Redis:
     return Redis(
-        host="localhost",
+        host="redis",
         port=6379,
         db=0,
         decode_responses=True
